11fc09da8526e7bb0f7e78e79f611e0e27d233b0
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-08-31 10:35:15 +0530

    learning operators

---
+++ JS/JS-Basics/JS-Block/one.js
@@ -0,0 +1,13 @@
+var product_price = 625;
+var product_Name = "Printed Men Round Neck Black T-Shirt";
+var is_Discount_Avail = true;
+var freeDeliver;
+
+var size = ["S", "M", "L"];
+console.log(typeof product_price);
+console.log(typeof product_Name);
+console.log(typeof is_Discount_Avail);
+console.log(typeof freeDeliver);
+console.log("..........................");
+console.log(typeof size);
+console.log(size);
---
+++ JS/JS-Basics/JS-Block/two.js
@@ -0,0 +1,5 @@
+var x = 10;
+var y ='a';
+var result = x*y;
+
+console.log(result);
