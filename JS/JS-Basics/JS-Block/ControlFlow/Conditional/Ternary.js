9bfca322772944ba3537b6c8606789c6655e8102
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-09-01 10:33:37 +0530

    ternary op

---
+++ JS/JS-Basics/JS-Block/ControlFlow/Conditional/Ternary.js
@@ -0,0 +1,7 @@
+/*
+ Ternary Op 
+ ?   : 
+ condition ?  "true block" : "false block";
+
+*/
+"Vijay"?  console.log("GM"): console.log("GN");
---
+++ JS/JS-Basics/JS-Block/ControlFlow/Conditional/one.js
@@ -0,0 +1,14 @@
+var salary = 40000;
+var result = salary > 50000;
+console.log(result);
+
+if (result) {
+  console.log("Eligible for All");
+}
+else{
+console.log("Not Eligible")
+}
+
+/* if(condition){
+    //true block or if block
+} */
