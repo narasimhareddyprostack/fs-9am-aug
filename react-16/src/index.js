8f4dac6846269571d0e3d95e80eb72d38bc3b133
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-09-21 13:46:29 +0530

    OKIE

---
+++ react-16/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("RajniKanth"));
+
+//ReactDOM.render(1,2);
+//ReactDOM.render("What you want to render", "where you want to render");
