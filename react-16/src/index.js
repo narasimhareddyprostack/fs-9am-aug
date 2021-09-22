b25dc73a31e96b362df9a9bb93a04d3162a6e7f7
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-09-22 08:34:12 +0530

    okie.

--- react-16/src/index.js
+++ react-16/src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 ReactDOM.render(<App />, document.getElementById("psa"));
-
 //ReactDOM.render(1,2);
 //ReactDOM.render("What you want to render", "where you want to render");
